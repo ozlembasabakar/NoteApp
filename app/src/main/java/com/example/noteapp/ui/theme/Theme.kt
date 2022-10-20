93a6cade9cabc438d417c4cb63d1dab4ed078f5d
Ozlem Basabakar <ozlembasabakar@gmail.com>
2022-10-20 17:50:35 +0300

    add project structure and import libraries

---
+++ app/src/main/java/com/example/noteapp/ui/theme/Theme.kt
@@ -0,0 +1,36 @@
+package com.example.noteapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColorPalette = lightColors(
+    primary = Color.White,
+    background = LightGray,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = LightGray
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun NoteAppTheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
+
+    MaterialTheme(
+            colors = LightColorPalette,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
