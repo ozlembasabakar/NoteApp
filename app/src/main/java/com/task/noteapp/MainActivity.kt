a14101e4162f353a70cd40a15a050bb37c6cb4b4
Ozlem Basabakar <ozlembasabakar@gmail.com>
2023-05-08 13:35:34 +0300

    added necessary changes to gradle and AndroidManifest to be able to use jetpack compose in the project

--- app/src/main/java/com/task/noteapp/MainActivity.kt
+++ app/src/main/java/com/task/noteapp/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.task.noteapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.task.noteapp.theme.NoteAppTheme
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            NoteAppTheme() {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
