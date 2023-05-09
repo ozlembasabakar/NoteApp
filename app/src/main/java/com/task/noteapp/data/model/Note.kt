e9bf0d2ba12bb940878e42e6463f507364303f10
Ozlem Basabakar <ozlembasabakar@gmail.com>
2023-05-09 18:12:43 +0300

    fixed build error.

--- app/src/main/java/com/task/noteapp/data/model/Note.kt
+++ app/src/main/java/com/task/noteapp/data/model/Note.kt
@@ -1,14 +1,9 @@
 package com.task.noteapp.data.model
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "note")
 data class Note(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    @ColumnInfo val title: String = "",
-    @ColumnInfo val description: String = "",
-    @ColumnInfo val imageUrl: String = "",
-    @ColumnInfo val timestamp: Long = 0
+    val id: Int = 0,
+    val title: String = "",
+    val description: String = "",
+    val imageUrl: String = "",
+    val timestamp: Long = 0
 )
