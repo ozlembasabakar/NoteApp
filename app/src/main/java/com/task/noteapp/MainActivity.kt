7381b9306692a60d7a64bd12f65339b3d6a983f9
Ozlem Basabakar <ozlembasabakar@gmail.com>
2023-05-11 22:06:14 +0300

    added splash screen.

--- app/src/main/java/com/task/noteapp/MainActivity.kt
+++ app/src/main/java/com/task/noteapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.task.noteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.task.noteapp.theme.NoteAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,6 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NoteAppTheme {
                 NoteAppNavHost()
